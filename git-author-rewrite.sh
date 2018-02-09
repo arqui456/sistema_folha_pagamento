9c6e104b1d8cc9551fb13247544699c82a4dd218
arqui456 <arquimedes9137@hotmail.com>
2018-02-09 15:22:04 -0300

    helper script

---
+++ git-author-rewrite.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="glnjunior@ic.ufal.br"
+CORRECT_NAME="arqui456"
+CORRECT_EMAIL="arquimedes9137@hotmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
